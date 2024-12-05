e27f4fa659a928df25b8f9e239272dca35428d31
Navpreet18 <navpreet1809@gmail.com>
2024-12-04 19:30:18 -0800

    Add files via upload

---
+++ src/UserList.jsx
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import { deleteUser } from './services/api';
+
+function UserList({ users, onEdit, fetchUsers }) {
+    const handleDelete = async (id) => {
+        await deleteUser(id);
+        fetchUsers();
+    };
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <th>ID</th>
+                    <th></th>
+                    <th>Email</th>
+                    <th>Actions</th>
+                </thead>
+                <tbody>
+                    {users.map(user => (
+                        <tr key={user._id}>
+                            <td>{user._id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>
+                                <button onClick={() => onEdit(user)}>Edit</button>
+                                <button onClick={() => handleDelete(user._id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+
+};
+
+export default UserList;
